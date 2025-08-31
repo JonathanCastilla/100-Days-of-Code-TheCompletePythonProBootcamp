9af87aaa682643e2fb07cf7f88a967fb46989edd
Jonathan E. Castilla <jonathancastilla.jecz@gmail.com>
2025-08-31 00:15:05 -0600

    Add comments to Tip Calculator project

--- Project2_Tip_Calculator/main.py
+++ Project2_Tip_Calculator/main.py
@@ -2,12 +2,16 @@
 
 # Defining function to print band name generated
 def tip_calculator():
-    print("Welcome to the Tip Calculator!")
+    print("Welcome to the Tip Calculator!") # Adding a greeting for this code
+    # Obtaining the data from user in order to calculate the bill for each one
     bill = float(input("What was the total bill? $"))
     tip = int(input("How much tip would you like to give? 10, 12, or 15%\n"))
     people = int(input("How many people to split the bill?\n"))
+
+    # Calculating the payment for each person
     payment_for_person = (bill * (1 + (tip / 100))) / people
 
+    # Printing the result
     print(f"Each person should pay: ${round(payment_for_person, 2)}")
 
 # Main function
