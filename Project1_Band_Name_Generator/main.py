23862e1643f4be445be3c3aacaa359a15ab4ab05
Jonathan E. Castilla <jonathancastilla.jecz@gmail.com>
2025-08-30 23:31:41 -0600

    Add comments to code

--- Project1_Band_Name_Generator/main.py
+++ Project1_Band_Name_Generator/main.py
@@ -1,12 +1,15 @@
 # This is a Band Name Generator
 
+# Defining function to print band name generated
 def band_name_generator():
     print("Welcome to the Band Name Generator!\n")
+    # Obtaining data from user
     city_name = input("Firstly, what is the name of the city you grew up in?\n")
     pet_name = input("What is your pet name?\n")
+    # Printing the band name generated
     print("Your band name could be: " + "'" + city_name + " " + pet_name + "'")
 
-
+# Main function
 if __name__ == '__main__':
-    band_name_generator()
+    band_name_generator() # Calling the function
 
